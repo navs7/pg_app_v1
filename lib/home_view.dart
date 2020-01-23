95a786f65727f592e7eb4d1d2d528f96095848c2
unknown <07navnee@gmail.com>
2020-01-23 16:37:44 +0530

    msg

---
+++ lib/home_view.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'pg_meals.dart';
+
+class HomeView extends StatelessWidget {
+  final List<PgMeals> _mealList = [
+    PgMeals(DateTime.now(), "Breakfast", "Not have", "Will have", "Single omlet", "Double omlet", "Nav", "Nav2", "Nav3", "Nav4", 4, 12, 5, 7),
+    PgMeals(DateTime.now(), "Lunch", "Not have", "Will have", "Single omlet", "Double omlet", "Nav", "Nav2", "Nav3", "Nav4", 3, 12, 8, 2),
+    PgMeals(DateTime.now(), "Dinner", "Not have", "Will have", "Single omlet", "Double omlet", "Nav", "Nav2", "Nav3", "Nav4", 5, 22, 1, 9),
+  ];
+  //var dateFormate = DateFormat("dd-MM-yyyy").format(DateTime.parse("2019-09-30"));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white30,
+      child: new ListView.builder(
+          itemCount: _mealList.length,
+          itemBuilder: (BuildContext context, int index) =>
+              buildDateCard(context, index)),
+    );
+  }
+
+  Widget buildDateCard(BuildContext context, int index) {
+    final meals = _mealList[index];
+    return Container(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top:8.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                        DateFormat("dd-MM-yyyy").format(meals.myDate).toString(),
+                        style: TextStyle(fontSize: 20.0),),
+                        Spacer(),
+                        Text(meals.mealTime, style: TextStyle(fontSize: 20.0),),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:8.0, bottom: 2.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.mealChoice1),
+                    Spacer(),
+                    Text(meals.totalCount1.toString()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:2.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.userNames1),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:8.0, bottom: 2.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.mealChoice2),
+                    Spacer(),
+                    Text(meals.totalCount2.toString()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:2.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.userNames2),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:8.0, bottom: 2.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.mealChoice3),
+                    Spacer(),
+                    Text(meals.totalCount3.toString()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:2.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.userNames3),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:8.0, bottom: 2.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.mealChoice4),
+                    Spacer(),
+                    Text(meals.totalCount4.toString()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:2.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(meals.userNames4),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pg_meals.dart
@@ -0,0 +1,20 @@
+class PgMeals{
+  final DateTime myDate;
+  final String mealTime;
+  final String mealChoice1;
+  final String mealChoice2;
+  final String mealChoice3;
+  final String mealChoice4;
+  final String userNames1;
+  final String userNames2;
+  final String userNames3;
+  final String userNames4;
+  final int totalCount1;
+  final int totalCount2;
+  final int totalCount3;
+  final int totalCount4;
+
+  PgMeals(this.myDate, this.mealTime, this.mealChoice1, this.mealChoice2, this.mealChoice3, this.mealChoice4,
+  this.userNames1, this.userNames2, this.userNames3, this.userNames4, this.totalCount1, this.totalCount2, this.totalCount3, this.totalCount4);
+
+}
